4ae8dd119580d88d8cfc8087ff044201758fa676
Hoon222 <46880753+Hoon222@users.noreply.github.com>
2021-09-02 22:25:09 +0900

    Delete cryptoauto directory

--- cryptoauto/test.py
+++
@@ -1,8 +0,0 @@
-import pyupbit
-
-access = "W1GrhxCCTYfLblVTNcXWzZKEpKh8XB8LOfYt6xsy"          # 본인 값으로 변경
-secret = "f9L0nYI73rlUtwrS6hbzVc00BCHx1TaA7CtG9l0g"          # 본인 값으로 변경
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-BTC"))     # KRW-XRP 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
